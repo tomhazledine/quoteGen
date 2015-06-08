1dd54d28e4eed160b3267c725a59da931400e8e7
Tom Hazledine <tom@eatenbymonsters.com>
2015-06-08 23:03:52 +0100

    switched to updated gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,10 @@ var cache        = require('gulp-cache');
 var scsslint     = require('gulp-scss-lint');
 var livereload   = require('gulp-livereload');
 var jshint       = require('gulp-jshint');
-var iconfont     = require('gulp-iconfont');
-var iconfontCss  = require('gulp-iconfont-css');
 var size         = require('gulp-size');
 var lr           = require('tiny-lr');
 var gutil        = require('gulp-util');
+var svgSprite    = require('gulp-svg-sprite');
 var plumber      = require('gulp-plumber');
 var server       = lr();
 
@@ -28,7 +27,7 @@ var onError = function (err) {
 
 // Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('uncompressed/scss/main.scss')
+    return gulp.src('_uncompressed/scss/**/*.scss')
         .pipe(sass({errLogToConsole: true}))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(size({title: 'css'}))
@@ -42,27 +41,27 @@ gulp.task('sass', function() {
 
 // Compile Our Critical Path Sass
 gulp.task('cpsass', function() {
-    return gulp.src('uncompressed/scss/criticalPath.scss')
+    return gulp.src('_uncompressed/scss/criticalPath.scss')
         .pipe(sass({errLogToConsole: true}))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(size({title: 'css'}))
-        .pipe(gulp.dest('assets/css/'))
+        .pipe(gulp.dest('_includes/'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
         .pipe(size({title: 'css.min'}))
-        .pipe(gulp.dest('assets/css/'))
+        .pipe(gulp.dest('_includes/'))
         // .pipe(livereload(server));
 });
 
 // Lets lint our CSS
 gulp.task('scss-lint', function() {
-    gulp.src('uncompressed/scss/*.scss')
+    gulp.src('_uncompressed/scss/*.scss')
         .pipe(scsslint({'config': 'defaultLint.yml'}));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src(['uncompressed/js/jquery/jquery.js','uncompressed/js/vendor/*.js','uncompressed/js/custom/*.js'])
+    return gulp.src(['_uncompressed/js/jquery/jquery.js','_uncompressed/js/vendor/*.js','_uncompressed/js/custom/*.js'])
         .pipe(plumber({
             errorHandler: onError
         }))
@@ -78,7 +77,7 @@ gulp.task('scripts', function() {
 
 // Minify and transfer static JS files
 gulp.task('staticjs', function() {
-    return gulp.src(['uncompressed/js/static/*.js'])
+    return gulp.src(['_uncompressed/js/static/*.js'])
         .pipe(plumber({
             errorHandler: onError
         }))
@@ -87,38 +86,15 @@ gulp.task('staticjs', function() {
         .pipe(livereload(server));
 });
 
-// Icon Font
-var fontName = 'icons';
-
-// Create icon fonts from SVG
-gulp.task('iconfont', function(){
-    gulp.src(['uncompressed/icons/*.svg'])
-        .pipe(iconfontCss({
-            fontName: fontName,
-            //path: 'app/assets/css/templates/_icons.scss',
-            targetPath: '../../scss/_icons.scss',
-            fontPath: 'uncompressed/fonts/icons/'
-        }))
-        .pipe(iconfont({
-            fontName: fontName,
-            appendCodepoints:true
-        }))
-        .on('codepoints', function(codepoints, options) {
-            // CSS templating, e.g.
-            console.log(codepoints, options);
-        })
-        .pipe(gulp.dest('uncompressed/fonts/icons/'));
-});
-
 gulp.task('jslint', function() {
-    return gulp.src('uncompressed/js/custom/*.js')
+    return gulp.src('_uncompressed/js/custom/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Set up image minification
 gulp.task('images', function() {
-    return gulp.src('uncompressed/images/**')
+    return gulp.src('_uncompressed/images/**')
         .pipe(imagemin({ optimizationLevel: 9, progressive: true, interlaced: true }))
         .pipe(gulp.dest('assets/images'))
         .pipe(livereload(server));
@@ -126,8 +102,26 @@ gulp.task('images', function() {
 
 // Fonts
 gulp.task('fonts', function() {
-    return gulp.src('uncompressed/fonts/**')
+    return gulp.src('_uncompressed/fonts/**')
         .pipe(gulp.dest('assets/fonts/'));
+});
+
+// SVG Icon Sprite
+var svgConfig = {
+    shape : {
+        dimension : { // Set maximum dimensions
+            maxWidth : 32,
+            maxHeight : 32
+        }
+    },
+    mode : {
+        symbol : true // Activate the «symbol» mode
+    }
+};
+gulp.task('svg',function(){
+    return gulp.src('_uncompressed/icons/**/*.svg')
+        .pipe(svgSprite(svgConfig))
+        .pipe(gulp.dest('_includes'));
 });
 
 // Livereload
@@ -141,14 +135,14 @@ gulp.task('listen', function(next) {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('uncompressed/js/jquery/*.js', ['scripts']);
-    gulp.watch('uncompressed/js/vendor/*.js', ['scripts']);
-    gulp.watch('uncompressed/js/custom/*.js', ['scripts']);
-    gulp.watch('uncompressed/js/static/*.js', ['staticjs']);
-    gulp.watch('uncompressed/scss/*.scss', ['sass']);
-    gulp.watch('uncompressed/images/**', ['images']);
-    gulp.watch('uncompressed/fonts/**', ['fonts']);
-    gulp.watch('uncompressed/icons/**', ['iconfont']);
+    gulp.watch('_uncompressed/js/jquery/*.js', ['scripts']);
+    gulp.watch('_uncompressed/js/vendor/*.js', ['scripts']);
+    gulp.watch('_uncompressed/js/custom/*.js', ['scripts']);
+    gulp.watch('_uncompressed/js/static/*.js', ['staticjs']);
+    gulp.watch('_uncompressed/scss/*.scss', ['sass']);
+    gulp.watch('_uncompressed/images/**', ['images']);
+    gulp.watch('_uncompressed/fonts/**', ['fonts']);
+    gulp.watch('_uncompressed/icons/**/*.svg', ['svg']);
 
     gulp.watch(['*.html','*.php']).on('change', function(file) {
         livereload(server).changed(file.path);
@@ -156,4 +150,4 @@ gulp.task('watch', function() {
 });
 
 // Default Task
-gulp.task('default', ['sass', 'scripts', 'images', 'fonts', 'iconfont', 'listen', 'watch']);
+gulp.task('default', ['sass', 'scripts', 'listen', 'watch']);
